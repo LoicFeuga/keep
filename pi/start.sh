8fad5b28b01edee0229082d10fd7b76928b727e6
LoicFeuga <7373407+LoicFeuga@users.noreply.github.com>
2020-11-09 12:11:13 +0100

    Update start.sh

--- pi/start.sh
+++ pi/start.sh
@@ -2,3 +2,6 @@ git pull https://${username}:${password}@gitlab/${username}/${project}
 npm install
 rm -rf dist
 npm run build
+ps -ef | grep ${project} | grep -v grep | awk '{print $2}' | xargs kill -9
+nohup npm run prod "${project}" &
+
